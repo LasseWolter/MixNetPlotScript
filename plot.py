124f692d0aaa58f3570fab6b373de2a78b90c35b
Lasse Wolter <wolter.lasse@gmail.com>
2019-07-26 18:48:43 +0100

    Two figures in one graph + saved to file
    + each figure has 2 x-axis (one in s, one in min)
    + next to the figures, mean and std of all server queues are displayed
    + plot is saved to file in the corresponding logDir

--- plot.py
+++ plot.py
@@ -5,6 +5,11 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Strips the directory and 'ql_' from the entries in node array
+def stripDir(s):
+    return s.split("_")[1]
+
+
 # Gets input args with default value none
 argNames = ["command", "logDir"]
 args = dict(zip(argNames, sys.argv))
@@ -15,8 +20,9 @@ timeStampFormat = '%b %d %H:%M:%S.%f'  # Timestamp format used in logFiles - lat
 zips = {}
 nodes = ["ql_mix1", "ql_provider", "ql_serviceprovider"]
 nodes = list(map(lambda a: args.logDir + "/" + a, nodes))  # prepends the lodDir to all files
+expName = args.logDir.split("/")[-1]  # Returns the experiment directory without it's full path
 
-# These arrays will contain the lengths and their corresponding time for the different mixnet-nodes
+#These arrays will contain the lengths and their corresponding time for the different mixnet-nodes
 times = {}
 lengths = {}
 
@@ -44,33 +50,75 @@ for nodeTimes in times.values():
     if nodeTimes[0] < startTime:  # looking at the first log entry from each file
         startTime = nodeTimes[0]
 
+# Find experiment end time following the same principle
+endTime = startTime
+for nodeTimes in times.values():
+    if nodeTimes[-1] > startTime:  # looking at the first log entry from each file
+        endTime = nodeTimes[-1]
+
+# Calculate experiment Duration in seconds
+expDuration = (endTime - startTime).total_seconds()
+
 # Make times relative to start time
 for node in times.keys():
     for i in range (0, len(times[node])):
-        times[node][i] = (times[node][i] - startTime).total_seconds() / 60   # time relative to start in minutes
+        times[node][i] = (times[node][i] - startTime).total_seconds()  # time relative to start in seconds 
 
-# Print figures
-# Original
-f1 = plt.figure()
-plt.title("Original")
-plt.xlabel("Time [min]")
-plt.ylabel("Queue Length")
+# Calculate Mean and standard deviation
+means = {}
+stds  = {}
 for node in nodes:
-    plt.plot(times[node], lengths[node])
+    means[node] = round(np.mean(lengths[node]), 2)
+    stds[node] = round(np.std(lengths[node]), 2)
+
+# Print figures 
+fSize=(18,10) # Size of figures
+minNum = int(expDuration/60) + 1 # How many full minutes happened during the experiment (+1 because of 0)
+minTicks = [x*60 for x in range(minNum)]
+winSize = 20  # Size of the convolution window
+textstr = ""
+
+# Original
+f1 = plt.figure(figsize=fSize)
+plt.subplots_adjust(left=0.25, hspace = 0.5)
+ax1 = f1.add_subplot(211)
+ax1.set_title("Original")
+ax1.set_xlabel("Time [s]")
+ax1.set_ylabel("Queue Length")
+
+# Add seconds axis showing full minutes
+ax2 = ax1.twiny()
+ax2.set_xticks(minTicks)
+ax2.set_xticklabels(list(range(minNum))) # list of numbers from 0..minNum
+ax2.set_xlabel("Time [min]")
+for node in nodes:
+    textstr += '\n'.join((
+        r'%s $\mu=%.2f$' % (stripDir(node),means[node]),
+        r'%s $\sigma=%.2f$' % (stripDir(node),stds[node]),
+        '',''))
+    ax1.plot(times[node], lengths[node])
+
+ax1.text(-0.25, 0.2, textstr, transform=ax1.transAxes)
 
 
 # Apply convolution with given window size
-f3 = plt.figure()
-winSize = 20  # Size of the convolution window
-plt.title("Convolution (winSize: %d)" % winSize)
-plt.xlabel("Time [min]")
-plt.ylabel("Queue Length")
+ax3 = f1.add_subplot(212)
+ax3.set_title("Convolution (winSize: %d)" % winSize)
+ax3.set_xlabel("Time [s]")
+ax3.set_ylabel("Queue Length")
+# Add seconds axis showing full minutes
+ax4 = ax3.twiny()
+ax4.set_xticks(minTicks)
+ax4.set_xticklabels(list(range(minNum))) # list of numbers from 0..minNum
+ax4.set_xlabel("Time [min]")
 for node in nodes:
     y = np.array(lengths[node])
     x = np.linspace(0, 500, num=len(y))
     f = np.convolve(y, np.ones(winSize), mode='same') / winSize
-    plt.plot(times[node], f)
+    ax3.plot(times[node], f)
 
-f1.show()
-f3.show()
-input()  # Just used to keep the figures alive
+f1.savefig("%s/%s.pdf" % (args.logDir, expName))
+f1.savefig("%s/%s.png" % (args.logDir, expName))
+# In case you want to display the figures
+#f1.show()
+#input()  # Just used to keep the figures alive
